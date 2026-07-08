68336f6f390ded0c6ccdb3a99853d8d2242508d4
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-08 08:48:09 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// build.gradle.kts (project level)
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
