20807afb69f1bf34177d5614de9e05e20e005f29
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-08 08:37:24 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    implementation("androidx.camera:camera-camera2:1.4.0")
+    implementation("androidx.camera:camera-lifecycle:1.4.0")
+    implementation("androidx.camera:camera-view:1.4.0")
+
+    implementation("org.opencv:opencv:4.10.0")  // Add OpenCV Android SDK
+    implementation("org.tensorflow:tensorflow-lite:2.17.0")
+    // implementation("org.tensorflow:tensorflow-lite-gpu:2.17.0") // optional
+}
